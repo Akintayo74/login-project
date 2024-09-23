650824754fe457a0023da844a33292acd3025786
Akintayo74 <127737541+Akintayo74@users.noreply.github.com>
2024-09-23 01:29:10 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+import { defineConfig } from 'vite';
+
+
 export default {
     base: '/login-project/', // Replace 'repository-name' with your GitHub repository name
+    build: {
+        outDir: 'dist', // Output directory for build files
+      },
   };
   
